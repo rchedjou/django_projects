3d17a8b99cebacec277517b067441f4d630e3546
rchedjou <chedjou.rocelin@gmail.com>
2023-01-11 05:29:53 +0100

    Chapitre_1:Building a Blog Application

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, id):
+    # try:
+    #     post=Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html',{'post':post})
+# Create your views here.
