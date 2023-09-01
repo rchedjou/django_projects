339adb8fd84d4d93cddaa78ccdfd2cbbf47d877f
rchedjou <chedjou.rocelin@.com>
2023-09-01 14:12:04 +0100

    Building an Online Shop

---
+++ myshop/celery.py
@@ -0,0 +1,8 @@
+import os
+import celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+app = celery.Celery('myshop')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
